40d4de69fb3fc8c4eb06d647abf26128b1c4caf0
demos1981 <50639080+demos1981@users.noreply.github.com>
2023-07-14 15:56:07 +0300

    commit2

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./index.html"],
   darkMode:'class',
   theme: {
+    container:{
+      center:true,
+    },
+    borderRadius:{
+      'none':'0',
+      'sm':'0.12rem',
+      DEFAULT:'0.25rem',
+      DEFAULT:'4px',
+      'md':'0.37rem',
+      'lg':'0.5rem',
+      'full':'9999px',
+      'large':'12px',
+    },
     extend: {},
   },
   plugins: [],
